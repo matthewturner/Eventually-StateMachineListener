e5224150945730f95d7db19ae47644087b76c599
Matt Turner <matt@fiverings.uk>
2022-12-28 19:45:10 +0000

    Improve example

--- examples/StateMachine/StateMachine.ino
+++ examples/StateMachine/StateMachine.ino
@@ -1,11 +1,14 @@
 #include <Eventually.h>
-#include "EvtStateMachineListener.h"
+#include <EvtStateMachineListener.h>
 
 #include <Arduino.h>
 
 const byte IDLE = 0;
 const byte PENDING = 1;
 const byte IN_PROGRESS = 2;
+
+EvtManager mgr;
+EvtStateMachineListener stateMachine;
 
 void wakeUp()
 {
@@ -32,12 +35,9 @@ bool inProgress()
     return true;
 }
 
-EvtManager mgr;
-StateMachineListener stateMachine;
-
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial)
         ;
 
@@ -45,9 +45,9 @@ void setup()
 
     stateMachine.transition(IDLE);
 
-    stateMachine.when(IDLE, (EvtAction)idle, PENDING, STATE_FAILED, 500);
+    stateMachine.when(IDLE, (EvtAction)idle, PENDING, STATE_FAILED, 5000);
     stateMachine.when(PENDING, (EvtAction)pending, IN_PROGRESS);
-    stateMachine.when(IN_PROGRESS, (EvtAction)inProgress, IDLE, STATE_FAILED, 500);
+    stateMachine.when(IN_PROGRESS, (EvtAction)inProgress, IDLE, STATE_FAILED, 2000);
     stateMachine.whenInterrupted(IDLE, PENDING);
 
     mgr.addListener(&stateMachine);
