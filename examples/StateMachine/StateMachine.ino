d9fca3fc313a99cc70c6164cd2bbbd3b1b29ef5d
Matt Turner <matt@fiverings.uk>
2022-12-28 19:49:53 +0000

    Improve example (#1)

--- examples/StateMachine/StateMachine.ino
+++ examples/StateMachine/StateMachine.ino
@@ -37,7 +37,7 @@ bool inProgress()
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial)
         ;
 
@@ -45,9 +45,9 @@ void setup()
 
     stateMachine.transition(IDLE);
 
-    stateMachine.when(IDLE, (EvtAction)idle, PENDING, STATE_FAILED, 500);
+    stateMachine.when(IDLE, (EvtAction)idle, PENDING, STATE_FAILED, 5000);
     stateMachine.when(PENDING, (EvtAction)pending, IN_PROGRESS);
-    stateMachine.when(IN_PROGRESS, (EvtAction)inProgress, IDLE, STATE_FAILED, 500);
+    stateMachine.when(IN_PROGRESS, (EvtAction)inProgress, IDLE, STATE_FAILED, 2000);
     stateMachine.whenInterrupted(IDLE, PENDING);
 
     mgr.addListener(&stateMachine);
