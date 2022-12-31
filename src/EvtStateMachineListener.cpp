f4bf421b2d7fce6ae3a5d8af56c04e19e3de4365
Matt Turner <matt@fiverings.uk>
2022-12-31 15:44:35 +0000

    Conditionally include disable listeners

--- src/EvtStateMachineListener.cpp
+++ src/EvtStateMachineListener.cpp
@@ -98,10 +98,12 @@ void EvtStateMachineListener::whenInterrupted(byte guardState, byte targetState)
 
 bool EvtStateMachineListener::isEventTriggered()
 {
+#ifdef EVT_SUPPORTS_DISABLE
     if (!EvtListener::isEventTriggered())
     {
         return false;
     }
+#endif
 
     return _state != STATE_FAILED;
 }
